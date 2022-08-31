e54e612eb4605ffed8619b8bc4817e9ef0e05278
86magical <86magical@gmail.com>
2022-08-31 03:21:03 +0100

    👟--Motion comp now support initial,animate prop

--- src/components/Motion.js
+++ src/components/Motion.js
@@ -1,32 +1,18 @@
 //@ts-nocheck
 import { createElement, memo, useLayoutEffect, useRef } from 'react'
 import { createMagicalSpringAnimation } from '../utility'
-
- const Motion =  (
-    { as,
+const defaultAnimate = {x:0,y:0}
+const Motion =  (
+    { 
+        as,
         children,
-        transition = {
-            delay: 0,
-            repeat: 1,
-            
-        },
-        configure = {
-            mass: 1,
-            stiffness: 100,
-            damping: 10,
-        },
-        animate = { // For when <Motion/> is Mounted
-            x: 0,
-            y: 0,
-            scale: 0,
-            scaleX: 0,
-            scaleY: 0,
-            rotate: 0,
-            opacity:{}
-        },
+        transitions = {},
+        configure={} ,
+        initial,
+        animate=defaultAnimate,
         onFinish,
-        ...otherProps 
-    }
+        ...otherProps
+     }
 ) =>
 {
 
@@ -36,26 +22,17 @@ import { createMagicalSpringAnimation } from '../utility'
     useLayoutEffect( () =>
     {
       if(isMountedOnce.current){
-        createMagicalSpringAnimation( motion, {
-            x: animate.x,
-            y: animate.y,
-            scale: animate.scale,
-            scaleX: animate.scaleX,
-            scaleY: animate.scaleY,
-            rotate: animate.rotate,
-            stiffness: configure.stiffness || 100,
-            damping: configure.damping||10,
-            mass: configure.mass || 1,
-            delay: transition.delay,
-            repeat: transition.repeat,
+          createMagicalSpringAnimation( motion, {
+        initial,
+        animate,
+        configure,
+        transitions,
+        others:{
             onFinish: onFinish,
-            FPS:configure.FPS,
-            direction: transition.direction,
-            // @ts-ignore
-            opacity:{from: animate.opacity.from , to:  animate.opacity.to } 
+        }
         } )
-      }
-       return ()=>{
+    }
+      return ()=>{
        isMountedOnce.current=false
        }
     }, [] )
@@ -63,7 +40,5 @@ import { createMagicalSpringAnimation } from '../utility'
         createElement( Component, { ref: motion, children, ...otherProps } )
     )
 }
-
- 
 
 export default memo(Motion)
